e0e5e2be873fe482edd1066443e9a5de6400b192
Alex Tercete <alex.tercete@red-gate.com>
2016-07-16 22:10:35 +0100

    Add [EmailAddressTypeId] to [Person].[EmailAddress]

---
+++ AdventureWorks.Database/Migrations/0005_Person.EmailAddress_AddEmailAddressTypeIdColumn.sql
@@ -0,0 +1,19 @@
+﻿-- <Migration ID="9434b01b-7e99-47ac-80bb-156ad4f5568a" />
+GO
+
+PRINT N'Altering [Person].[EmailAddress]'
+GO
+ALTER TABLE [Person].[EmailAddress] ADD
+[EmailAddressTypeID] [int] NULL CONSTRAINT [DF_EmailAddress_EmailAddressTypeID] DEFAULT ((1))
+GO
+
+PRINT N'Adding foreign keys to [Person].[EmailAddress]'
+GO
+ALTER TABLE [Person].[EmailAddress] ADD CONSTRAINT [FK_EmailAddress_EmailAddressType_EmailAddressTypeID] FOREIGN KEY ([EmailAddressTypeID]) REFERENCES [Person].[EmailAddressType] ([EmailAddressTypeID])
+GO
+
+PRINT N'Populating existing rows with default value'
+UPDATE [Person].[EmailAddress]
+    SET [EmailAddressTypeID] = 1
+    WHERE [EmailAddress] IS NOT NULL
+GO
