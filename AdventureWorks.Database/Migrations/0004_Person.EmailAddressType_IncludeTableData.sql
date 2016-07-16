e58594560642615bff8d3056926b2ce271351133
Alex Tercete <alex.tercete@red-gate.com>
2016-07-16 22:10:34 +0100

    Include table data for [Person].[EmailAddressType]

---
+++ AdventureWorks.Database/Migrations/0004_Person.EmailAddressType_IncludeTableData.sql
@@ -0,0 +1,25 @@
+﻿-- <Migration ID="b2666d52-b6e2-41ca-a0ff-a472c01617ff" />
+GO
+
+SET DATEFORMAT YMD;
+
+
+GO
+DECLARE @pv AS BINARY (16);
+
+
+GO
+IF (SELECT COUNT(*)
+    FROM   [Person].[EmailAddressType]) = 0
+    BEGIN
+        PRINT (N'Add 2 rows to [Person].[EmailAddressType]');
+        SET IDENTITY_INSERT [Person].[EmailAddressType] ON;
+        INSERT  INTO [Person].[EmailAddressType] ([EmailAddressTypeID], [Name], [ModifiedDate])
+        VALUES                                  (1, N'Primary', '2016-06-09 11:53:42.037');
+        INSERT  INTO [Person].[EmailAddressType] ([EmailAddressTypeID], [Name], [ModifiedDate])
+        VALUES                                  (2, N'Secondary', '2016-06-09 11:53:42.040');
+        SET IDENTITY_INSERT [Person].[EmailAddressType] OFF;
+    END
+
+
+GO
