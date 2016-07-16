5f3f1b72cd9cc1f5dc21c6c1b01fa2b1c9ca3070
Alex Tercete <alex.tercete@red-gate.com>
2016-07-16 22:10:36 +0100

    Add [ChangedDate] to [Person].[Address]

---
+++ AdventureWorks.Database/Migrations/0006_Person.Address_AddCreatedDateColumn.sql
@@ -0,0 +1,29 @@
+﻿-- <Migration ID="b1823fac-5f84-4498-a57d-d413dcf71c4b" />
+GO
+
+PRINT N'Adding [Person].[Address].[CreatedDate] column (empty)'
+GO
+ALTER TABLE [Person].[Address]
+    ADD [CreatedDate] [datetime] NULL
+GO
+
+PRINT N'Populating existing rows with data from the [ModifiedDate] column'
+GO
+UPDATE [Person].[Address]
+    SET [CreatedDate] = [ModifiedDate]
+    WHERE [CreatedDate] IS NULL
+GO
+
+PRINT N'Adding default constraint...'
+GO
+ALTER TABLE [Person].[Address]
+    ADD CONSTRAINT [DF_Address_CreatedDate]
+    DEFAULT (getdate())
+    FOR [CreatedDate]
+GO
+
+PRINT N'Setting column to NOT NULL'
+GO
+ALTER TABLE [Person].[Address]
+    ALTER COLUMN [CreatedDate] [datetime] NOT NULL
+GO
