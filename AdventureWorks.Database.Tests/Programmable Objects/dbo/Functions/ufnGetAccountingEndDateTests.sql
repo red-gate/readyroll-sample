5e1bbe81c3120fe3abf816d8c409d8f327800c7b
Alex Tercete <alex.tercete@red-gate.com>
2016-09-01 11:05:48 +0100

    Write some simple unit tests as a proof of concept

---
+++ AdventureWorks.Database.Tests/Programmable Objects/dbo/Functions/ufnGetAccountingEndDateTests.sql
@@ -0,0 +1,15 @@
+﻿-- <Migration ID="565a2147-9a0d-4e91-8494-1306ca209e6f" />
+EXEC tSQLt.NewTestClass 'ufnGetAccountingEndDateTests';
+GO
+
+CREATE PROCEDURE ufnGetAccountingEndDateTests.[test that correct accounting end date is returned]
+AS
+BEGIN
+    DECLARE @actual DATETIME;
+    SELECT @actual = [dbo].ufnGetAccountingEndDate();
+
+    DECLARE @expected DATETIME;
+    SET @expected = '2004-06-30 23:59:59:998';
+    EXEC tSQLt.AssertEquals @expected, @actual;
+END;
+GO
---
+++ AdventureWorks.Database.Tests/Programmable Objects/dbo/Functions/ufnGetAccountingStartDateTests.sql
@@ -0,0 +1,15 @@
+﻿-- <Migration ID="f3829be9-eeae-4d48-859a-5c1056d78a32" />
+EXEC tSQLt.NewTestClass 'ufnGetAccountingStartDateTests';
+GO
+
+CREATE PROCEDURE ufnGetAccountingStartDateTests.[test that correct accounting start date is returned]
+AS
+BEGIN
+    DECLARE @actual DATETIME;
+    SELECT @actual = [dbo].ufnGetAccountingStartDate();
+
+    DECLARE @expected DATETIME;
+    SET @expected = '2003-07-01';
+    EXEC tSQLt.AssertEquals @expected, @actual;
+END;
+GO
