d941c8baf84cd74caf6fb309e9b4037f388ecafe
Mircea Dogaru <mircea.dogaru@red-gate.com>
2017-01-27 11:52:20 +0000

    Person table change

---
+++ AdventureWorks.Database/Migrations/0004_20170127-1150_Mircea.Dogaru.sql
@@ -0,0 +1,9 @@
+﻿-- <Migration ID="31474244-3eb9-4ed9-a0a8-f1d8dcdd15c3" />
+GO
+
+PRINT N'Altering [Person].[Person]'
+GO
+ALTER TABLE [Person].[Person] ADD
+[CreationDate] [datetime] NULL,
+[TestField] [nchar] (10) NULL
+GO
